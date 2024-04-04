33a6875b2ff6e3bfa3b3a1f32f6e841dcf5e1944
Andar Pratama <andar.pratama@deushcs.com>
2024-04-04 20:47:31 +0700

    update connection

--- models/connection.js
+++ models/connection.js
@@ -2,11 +2,15 @@ import mysql from 'mysql'
 import dbConfig from '../configs/db.config.js'
 
 const connection = mysql.createPool({
+    connectionLimit: 10,
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DB,
-    port: dbConfig.PORT
+    port: dbConfig.PORT,
+    connectTimeout: 20000,
+    waitForConnections: true,
+    queueLimit: 0
 })
 
 export default connection
